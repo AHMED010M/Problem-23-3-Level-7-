78a108ed09901627418ba2682009ceebd7659113
Abo Salah <91723027+AHMED010M@users.noreply.github.com>
2025-07-03 14:12:01 +0300

    Add files via upload

---
+++ Problem #23.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadNameFromUser()
+{
+    string name;
+    cout << "Please Enter Your String?" << endl;
+    getline(cin, name);
+
+    return name;
+}
+
+
+void PrintFistLetterFromString(string name)
+{
+
+    bool IsFisrtLetter = true;
+
+    for (int i = 0; i<name.length(); i++)
+    {
+      
+      
+
+        if (name[i] != ' ' && IsFisrtLetter)
+        {
+            cout << name[i] << endl;
+
+        }
+        IsFisrtLetter = (name[i] == ' ' ? true : false);
+
+    }
+
+
+}
+
+int main()
+{
+    
+    PrintFistLetterFromString(ReadNameFromUser());
+
+
+}
+
